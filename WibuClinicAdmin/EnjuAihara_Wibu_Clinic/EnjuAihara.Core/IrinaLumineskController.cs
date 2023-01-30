3285cf1893290b6f3b4ed6c9bb8d29e3f349a6ff
IrinaLuminesk <1951012084nhan@ou.edu.vn>
2023-01-30 11:15:40 +0700

    Thêm BaseController

--- WibuClinicAdmin/EnjuAihara_Wibu_Clinic/EnjuAihara.Core/IrinaLumineskController.cs
+++ WibuClinicAdmin/EnjuAihara_Wibu_Clinic/EnjuAihara.Core/IrinaLumineskController.cs
@@ -4,9 +4,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Web.Mvc;
+using EnjuAihara.EntityFramework;
 namespace EnjuAihara.Core
 {
     public class IrinaLumineskController : System.Web.Mvc.Controller
     {
+        public QuanLyPhongMachWibuEntities _context = new QuanLyPhongMachWibuEntities();
+
+        protected IrinaLumineskController()
+        {
+            _context = new QuanLyPhongMachWibuEntities();
+        }
     }
 }
--- WibuClinicAdmin/EnjuAihara_Wibu_Clinic/EnjuAihara_Wibu_Clinic_Main/Areas/Permission/Controllers/AuthController.cs
+++ WibuClinicAdmin/EnjuAihara_Wibu_Clinic/EnjuAihara_Wibu_Clinic_Main/Areas/Permission/Controllers/AuthController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using EnjuAihara.Core;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -6,7 +7,7 @@ using System.Web.Mvc;
 
 namespace Permission.Controllers
 {
-    public class AuthController : Controller
+    public class AuthController : IrinaLumineskController
     {
         // GET: Auth
         public ActionResult Index()
