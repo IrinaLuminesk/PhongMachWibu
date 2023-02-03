6e2d58236fd4397192b15122ee5b648ec463ede5
To Quy Phuoc <1951012094phuoc@ou.edu.vn>
2023-02-03 23:34:18 +0700

    Xong trang quản lý thao tác

--- WibuClinicAdmin/EnjuAihara_Wibu_Clinic/EnjuAihara_Wibu_Clinic_Main/Areas/Permission/Controllers/FunctionController.cs
+++ WibuClinicAdmin/EnjuAihara_Wibu_Clinic/EnjuAihara_Wibu_Clinic_Main/Areas/Permission/Controllers/FunctionController.cs
@@ -21,8 +21,10 @@ namespace EnjuAihara_Wibu_Clinic_Main.Areas.Permission.Controllers
         public PartialViewResult _Search(FunctionModel search)
         {
             List<FunctionModel> kq = _context.FunctionModels.Where(x=>
-                (x.FunctionId.Equals(search.FunctionId))
-                || (x.FunctionName.Equals(search.FunctionName))
+                (x.FunctionId.Contains(search.FunctionId))
+                || (x.FunctionName.Contains(search.FunctionName))
+                || (string.IsNullOrEmpty(search.FunctionName))
+                || (string.IsNullOrEmpty(search.FunctionId))
                 ).ToList();
             return PartialView(kq);
         }
@@ -30,14 +32,108 @@ namespace EnjuAihara_Wibu_Clinic_Main.Areas.Permission.Controllers
         {
             return View();
         }
+        [HttpPost]
+        public JsonResult Create(FunctionModel model)
+        {
+            //Báo lỗi
+            JsonResult json = ValidateFunction(model);
+            if (json != null)
+            {
+                return json;
+            }
+            try
+            {
+                FunctionModel function = new FunctionModel()
+                {
+                    FunctionName = model.FunctionName,
+                    FunctionId = model.FunctionId
+                };
+                _context.FunctionModels.Add(function);
+                _context.SaveChanges();
+                return Json(new
+                {
+                    isSucess = true,
+                    title = "Thành công",
+                    message = "Tạo Function mới thành công",
+                    redirect = "/Permission/Function"
+                });
+            }catch(Exception ex)
+            {
+                return Json(new
+                {
+                    isSucess = false,
+                    title = "Lỗi",
+                    message = "Tạo Function mới thất bại",
+                    redirect = "/Permission/Function"
+                });
+            }
+        }
+        public JsonResult ValidateFunction(FunctionModel model)
+        {
+            if (model.FunctionId == "")
+            {
+                return Json(new
+                {
+                    isSucess = false,
+                    title = "Lỗi",
+                    message = "Vui lòng không để trống mã chức năng"
+                });
+            }
+            if (model.FunctionName == "")
+            {
+                return Json(new
+                {
+                    isSucess = false,
+                    title = "Lỗi",
+                    message = "Vui lòng không để trống tên chức năng"
+                });
+            }
+            return null;
+        }
+        public ActionResult Edit(string id)
+        {
+            var function = _context.FunctionModels.FirstOrDefault(x => x.FunctionId.Equals(id));
+            return View(function);
+        }
+        [HttpPost]
+        public JsonResult Edit(FunctionModel model)
+        {
+            //Báo lỗi
+            JsonResult json = ValidateFunction(model);
+            if (json != null)
+            {
+                return json;
+            }
+            try
+            {
+                FunctionModel function = new FunctionModel()
+                {
+                    FunctionName = model.FunctionName,
+                    FunctionId = model.FunctionId
+                };
+                _context.Entry(function).State=EntityState.Modified;
+                _context.SaveChanges();
+                return Json(new
+                {
+                    isSucess = true,
+                    title = "Thành công",
+                    message = "Lưu Function thành công",
+                    redirect = "/Permission/Function"
+                });
+            }
+            catch (Exception ex)
+            {
+                return Json(new
+                {
+                    isSucess = false,
+                    title = "Lỗi",
+                    message = "Lưu Function thất bại",
+                    redirect = "/Permission/Function"
+                });
+            }
+        }
         public void CreateViewBag()
         {
-            List<SelectBoolItem> StatusList = new List<SelectBoolItem>()
-            {
-                new SelectBoolItem() { id = true, name = "Đang sử dụng"},
-                new SelectBoolItem() { id = false, name = "Ngừng sử dụng"}
-            };
-            ViewBag.Actived = new SelectList(StatusList, "id", "name");
         }
     }
 }
