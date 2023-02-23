40318298129b33f93d0d9a3a466f1fa259de7d11
To Quy Phuoc <1951012094phuoc@ou.edu.vn>
2023-02-23 15:50:59 +0700

    Tạo mới controller

---
+++ WibuClinicAdmin/EnjuAihara_Wibu_Clinic/EnjuAihara_Wibu_Clinic_Main/Areas/MasterData/Controllers/ProviderController.cs
@@ -0,0 +1,30 @@
+﻿using EnjuAihara.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EnjuAihara_Wibu_Clinic_Main.Areas.MasterData.Controllers
+{
+    public class ProviderController : IrinaLumineskController
+    {
+        // GET: MasterData/Provider
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+        public ActionResult Edit(Guid id)
+        {
+            return View();
+        }
+        public void CreateViewBag()
+        {
+
+        }
+    }
+}
