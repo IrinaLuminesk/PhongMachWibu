f2de2fedd59eec29bdc36ca6308bf9ecb9746fd0
To Quy Phuoc <1951012094phuoc@ou.edu.vn>
2023-02-27 13:53:47 +0700

    Xong create

---
+++ WibuClinicAdmin/EnjuAihara_Wibu_Clinic/EnjuAihara.ViewModels/MasterData/ProviderSearchViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnjuAihara.ViewModels.MasterData
+{
+    public class ProviderSearchViewModel
+    {
+        //Phân trang
+        public int? PageSize { get; set; }
+        public int? PageNumber { get; set; }
+        //Thông số search
+        public string ProviderNameSearch { get; set; }
+        public string ProviderCodeSearch { get; set; }
+        public string AddressSearch { get; set; }
+        public bool Actived { get; set; }
+
+        public int STT { get; set; }
+        public Guid ProviderId { get; set; }
+        public string ProviderName { get; set; }
+        public string ProviderCode { get; set; }
+        public string Address {get; set; }
+        public string Status { get; set; }
+        public float Latidude { get; set; }
+        public float Longitude { get; set; }
+
+    }
+}
--- WibuClinicAdmin/EnjuAihara_Wibu_Clinic/EnjuAihara_Wibu_Clinic_Main/Areas/MasterData/Controllers/ProviderController.cs
+++ WibuClinicAdmin/EnjuAihara_Wibu_Clinic/EnjuAihara_Wibu_Clinic_Main/Areas/MasterData/Controllers/ProviderController.cs
@@ -1,7 +1,18 @@
 ﻿using EnjuAihara.Core;
+using EnjuAihara.EntityFramework;
+using EnjuAihara.Utilities.CloudinaryHelper;
+using EnjuAihara.Utilities.Datatable;
+using EnjuAihara.Utilities.DateTimeFormat;
+using EnjuAihara.Utilities.EncryptionAlgorithm;
+using EnjuAihara.Utilities.RandomString;
+using EnjuAihara.Utilities.SelectListItemCustom;
+using EnjuAihara.ViewModels.Datatable;
+using EnjuAihara.ViewModels.MasterData;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Web;
 using System.Web.Mvc;
 
@@ -12,11 +23,106 @@ namespace EnjuAihara_Wibu_Clinic_Main.Areas.MasterData.Controllers
         // GET: MasterData/Provider
         public ActionResult Index()
         {
+            CreateViewBag();
             return View();
+        }
+        public JsonResult _PaggingServerSide(DatatableViewModel model, ProviderSearchViewModel search, string ProviderCodeSearch, string ProviderNameSearch,string AddressSearch , bool? Actived)
+        {
+            int filteredResultsCount;
+            int totalResultsCount = model.length;
+
+            //if (ToDate != null)
+            //    ToDate = ((DateTime)ToDate).AddDays(1).AddSeconds(-1);
+
+            search.PageSize = model.length;
+            search.PageNumber = model.start / model.length + 1;
+
+            var query = _context.ProviderModels.
+                Where(x => (x.ProviderCode.Contains(ProviderCodeSearch) || string.IsNullOrEmpty(ProviderCodeSearch))
+                && (x.ProviderName.Contains(ProviderNameSearch) || string.IsNullOrEmpty(ProviderNameSearch))
+                && (x.Address.Contains(AddressSearch) || string.IsNullOrEmpty(AddressSearch))
+                //&& (x.AccountInRoleModels.Any(z => z.RoleId == RoleNameSearch) || RoleNameSearch == null)
+                && (x.Actived == Actived || Actived == null)
+                //&& (x.CreateDate >= FromDate || FromDate == null)
+                //&& (x.CreateDate <= ToDate || ToDate == null)
+                )
+                .Select(x =>
+            new ProviderSearchViewModel
+            {
+                ProviderId = x.ProviderId,
+                ProviderCode = x.ProviderCode,
+                ProviderName = x.ProviderName,
+                //RoleName = x.RolesModels.Select(y => y.RoleName).ToList(),
+                //RoleName = x.AccountInRoleModels.Select(y => y.RolesModel.RoleName).ToList(),
+                Address=x.Address,
+                Status=x.Actived==true? "Đang sử dụng":"Đã ngừng",
+                
+
+            }).ToList();
+            var finalResult = PaggingServerSideDatatable.DatatableSearch<ProviderSearchViewModel>(model, out filteredResultsCount, out totalResultsCount, query.AsQueryable(), "STT");
+            if (finalResult != null && finalResult.Count > 0)
+            {
+                int i = model.start;
+                foreach (var item in finalResult)
+                {
+                    i++;
+                    item.STT = i;
+                    //item.CreateDateString = FormatDateTime.FormatDateTimeWithString(item.CreateDate);
+                    //item.LastLoginTimeString = FormatDateTime.FormatDateTimeWithString(item.LastLoginTime);
+                }
+            }
+            return Json(new
+            {
+                draw = model.draw,
+                recordsTotal = totalResultsCount,
+                recordsFiltered = filteredResultsCount,
+                data = finalResult
+            });
         }
         public ActionResult Create()
         {
+            CreateViewBag();
             return View();
+        }
+        [HttpPost]
+        public JsonResult Create(ProviderModel model)
+        {
+            if (string.IsNullOrEmpty(model.ProviderName))
+            {
+                return Json(new
+                {
+                    isSucess = false,
+                    title = "Lỗi",
+                    message = "Vui lòng không để trống tên nhà cung cấp"
+                });
+            }
+            try {
+                ProviderModel newNcc = new ProviderModel()
+                {
+                    ProviderId = Guid.NewGuid(),
+                    ProviderCode = DataCodeGenerate.UserCodeGen(),
+                    Actived = true,
+                    ProviderName = model.ProviderName,
+                    
+                };
+                _context.Entry(newNcc).State = EntityState.Added;
+                _context.SaveChanges();
+                return Json(new
+                {
+                    isSucess = true,
+                    title = "Thành công",
+                    message = "Thêm tài khoản mới thành công"
+                });
+            }
+            catch (Exception ex)
+            {
+                return Json(new
+                {
+                    isSucess = false,
+                    title = "Lỗi",
+                    message = "Đã có lỗi xảy ra " + ex.Message.ToString()
+                });
+            }
         }
         public ActionResult Edit(Guid id)
         {
@@ -24,7 +130,7 @@ namespace EnjuAihara_Wibu_Clinic_Main.Areas.MasterData.Controllers
         }
         public void CreateViewBag()
         {
-
+            ViewBag.Actived = new SelectList(SelectListItemCustom.GetStatusSelectList(), "id", "name");
         }
     }
 }
