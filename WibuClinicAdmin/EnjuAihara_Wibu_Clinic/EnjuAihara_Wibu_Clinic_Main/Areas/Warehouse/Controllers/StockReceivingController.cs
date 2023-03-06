eb0f5ef9c8752384076003e448ea1ec138df1178
To Quy Phuoc <1951012094phuoc@ou.edu.vn>
2023-03-06 15:59:27 +0700

    update entity

---
+++ WibuClinicAdmin/EnjuAihara_Wibu_Clinic/EnjuAihara_Wibu_Clinic_Main/Areas/Warehouse/Controllers/StockReceivingController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EnjuAihara_Wibu_Clinic_Main.Areas.Warehouse.Controllers
+{
+    public class StockReceivingController : Controller
+    {
+        // GET: Warehouse/StockReceiving
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
