6339d0e7ed591db9f8cb39750b493feb8d439db5
To Quy Phuoc <1951012094phuoc@ou.edu.vn>
2023-03-23 14:21:40 +0700

    xong 2 báo cáo

---
+++ WibuClinicAdmin/EnjuAihara_Wibu_Clinic/EnjuAihara.ViewModels/Reports/SLKhachTheoThangViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnjuAihara.ViewModels.Reports
+{
+    public class SLKhachTheoThangViewModel
+    {
+        public int SLKhach { get; set; }
+        public int Thang { get; set; }
+    }
+}
---
+++ WibuClinicAdmin/EnjuAihara_Wibu_Clinic/EnjuAihara_Wibu_Clinic_Main/Scripts/ChartJs/Canvas1.js
@@ -0,0 +1,51 @@
+﻿function GetDataSLKhachTheoThang(chart, SLKhach) {
+    axios
+        .post("Home/GetSLKhachTheoThang")
+        .then(res => {
+            $.each(res.data, function (index, value) {
+                SLKhach.push(value.SLKhach);
+
+            });
+            chart.update();
+        })
+        .catch(error => console.log(error));
+}
+function ThongKeSLKhachTheoThang() {
+    let SLKhach=[];
+
+    const labels = ["Tháng 1", "Tháng 2", "Tháng 3", "Tháng 4", "Tháng 5", "Tháng 6", "Tháng 7", "Tháng 8", "Tháng 9", "Tháng 10", "Tháng 11", "Tháng 12"];
+    const data = {
+        labels: labels,
+        datasets: [
+            {
+                label: 'Số lượng khách',
+                data: SLKhach,
+                fill: false,
+                borderColor: '#feafbe',
+                backgroundColor: '#feafbe',
+                tension: 0.1
+            },
+
+        ]
+    };
+    const config = {
+        type: 'line',
+        data: data,
+        options: {
+            responsive: true,
+            plugins: {
+                title: {
+                    display: true,
+                    /*text: (ctx) => 'Point Style: ' + ctx.chart.data.datasets[0].pointStyle,*/
+                }
+            }
+        },
+        responsive: false,
+        maintainAspectRatio: false
+    };
+    var canvas = $("#canvas1");
+    const chart = new Chart(canvas, config);
+    GetDataSLKhachTheoThang(chart, SLKhach);
+    window.addEventListener('resize', function () { chart.resize() })
+}
+    
